c1392b910728ede10ed0a649f458f1d9080b512a
Fabia Serra Arrizabalaga <farrizabalaga@alkemy-x.com>
2023-08-07 17:32:29 +0200

    Add support to split render job in extract + render

--- openpype/modules/deadline/abstract_submit_deadline.py
+++ openpype/modules/deadline/abstract_submit_deadline.py
@@ -445,12 +445,26 @@ class AbstractSubmitDeadline(pyblish.api.InstancePlugin,
 
         self.scene_path = file_path
         self.log.info("Using {} for render/export.".format(file_path))
+        export_job = instance.data["exportJob"]
 
-        self.job_info = self.get_job_info()
+        self.job_info = self.get_job_info(split_render_job=export_job, export_job=True)
         self.plugin_info = self.get_plugin_info()
         self.aux_files = self.get_aux_files()
 
-        self.process_submission()
+        job_id = self.process_submission()
+        if export_job:
+            self.log.info("Splitting export and render in two jobs")
+            self.log.info("Export job id: %s", job_id)
+            render_job_info = self.get_job_info(
+                split_render_job=True, dependency_job_ids=[job_id]
+            )
+            render_plugin_info = self.get_plugin_info(split_render_job=True)
+            payload = self.assemble_payload(
+                job_info=render_job_info,
+                plugin_info=render_plugin_info
+            )
+            render_job_id = self.submit(payload)
+            self.log.info("Render job id: %s", render_job_id)
 
     def process_submission(self):
         """Process data for submission.
@@ -466,7 +480,7 @@ class AbstractSubmitDeadline(pyblish.api.InstancePlugin,
         return self.submit(payload)
 
     @abstractmethod
-    def get_job_info(self):
+    def get_job_info(self, split_render_job=False, export_job=False, dependency_job_ids=None):
         """Return filled Deadline JobInfo.
 
         This is host/plugin specific implementation of how to fill data in.
@@ -481,7 +495,7 @@ class AbstractSubmitDeadline(pyblish.api.InstancePlugin,
         pass
 
     @abstractmethod
-    def get_plugin_info(self):
+    def get_plugin_info(self, split_render_job=False):
         """Return filled Deadline PluginInfo.
 
         This is host/plugin specific implementation of how to fill data in.
