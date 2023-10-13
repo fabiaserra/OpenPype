1ad7c5f5ff14afd15d2c64aa96400668e285fdb3
Fabia Serra Arrizabalaga <farrizabalaga@alkemy-x.com>
2023-10-13 16:43:50 -0700

    Add first draft for outsource delivery

--- openpype/modules/delivery/tray/delivery_tray.py
+++ openpype/modules/delivery/tray/delivery_tray.py
@@ -4,6 +4,7 @@ from qtpy import QtWidgets
 
 from openpype.lib import Logger
 from openpype.modules.delivery.tray.delivery_dialog import DeliveryDialog
+from openpype.modules.delivery.tray.outsource_dialog import OutsourceDialog
 
 
 class DeliveryTrayWrapper:
@@ -12,17 +13,31 @@ class DeliveryTrayWrapper:
         self.log = Logger.get_logger(self.__class__.__name__)
 
         self.delivery_dialog = DeliveryDialog(module)
+        self.outsource_dialog = OutsourceDialog(module)
 
     def show_delivery_dialog(self):
         self.delivery_dialog.show()
         self.delivery_dialog.activateWindow()
         self.delivery_dialog.raise_()
 
+    def show_outsource_dialog(self):
+        self.outsource_dialog.show()
+        self.outsource_dialog.activateWindow()
+        self.outsource_dialog.raise_()
+
     def tray_menu(self, parent_menu):
         tray_menu = QtWidgets.QMenu("Delivery", parent_menu)
 
-        show_delivery_action = QtWidgets.QAction("Deliver SG Entities", tray_menu)
+        show_delivery_action = QtWidgets.QAction(
+            "Deliver SG Entities", tray_menu
+        )
         show_delivery_action.triggered.connect(self.show_delivery_dialog)
         tray_menu.addAction(show_delivery_action)
+
+        show_delivery_outsource_action = QtWidgets.QAction(
+            "Deliver for Outsource", tray_menu
+        )
+        show_delivery_outsource_action.triggered.connect(self.show_outsource_dialog)
+        tray_menu.addAction(show_delivery_outsource_action)
 
         parent_menu.addMenu(tray_menu)
---
+++ openpype/modules/delivery/tray/outsource_dialog.py
@@ -0,0 +1,307 @@
+import re
+import platform
+import traceback
+from qtpy import QtCore, QtWidgets, QtGui
+
+from openpype import style
+from openpype import resources
+from openpype.lib import Logger
+from openpype.client import get_projects
+from openpype.pipeline import AvalonMongoDB
+from openpype.tools.utils import lib as tools_lib
+from openpype.modules.shotgrid.lib import credentials
+from openpype.modules.delivery.scripts import sg_delivery
+
+
+logger = Logger.get_logger(__name__)
+
+
+class OutsourceDialog(QtWidgets.QDialog):
+    """Interface to control deliverying SG entities for outsource"""
+
+    tool_title = "Outsource Deliver SG Entities"
+    tool_name = "outsource_sg_entity_delivery"
+
+    SIZE_W = 800
+    SIZE_H = 400
+
+    def __init__(self, module, parent=None):
+        super(OutsourceDialog, self).__init__(parent)
+
+        self.setWindowTitle(self.tool_title)
+
+        self._module = module
+
+        icon = QtGui.QIcon(resources.get_openpype_icon_filepath())
+        self.setWindowIcon(icon)
+
+        self.setWindowFlags(
+            QtCore.Qt.WindowStaysOnTopHint
+            | QtCore.Qt.WindowCloseButtonHint
+            | QtCore.Qt.WindowMinimizeButtonHint
+        )
+
+        self.setMinimumSize(QtCore.QSize(self.SIZE_W, self.SIZE_H))
+
+        self.sg = credentials.get_shotgrid_session()
+
+        self._first_show = True
+        self._initial_refresh = False
+        self._ignore_project_change = False
+
+        # Short code name for currently selected project
+        self._current_proj_code = None
+
+        dbcon = AvalonMongoDB()
+        dbcon.install()
+        dbcon.Session["AVALON_PROJECT"] = None
+        self.dbcon = dbcon
+
+        self.ui_init()
+
+    def ui_init(self):
+
+        main_layout = QtWidgets.QVBoxLayout(self)
+
+        input_widget = QtWidgets.QWidget()
+
+        # Common input widgets for delivery and republish features
+        input_layout = QtWidgets.QFormLayout(input_widget)
+        input_layout.setContentsMargins(5, 5, 5, 5)
+
+        # Project combobox
+        projects_combobox = QtWidgets.QComboBox()
+        combobox_delegate = QtWidgets.QStyledItemDelegate(self)
+        projects_combobox.setItemDelegate(combobox_delegate)
+        projects_combobox.currentTextChanged.connect(self.on_project_change)
+        input_layout.addRow("Project", projects_combobox)
+
+        # SG input widgets
+        sg_input_widget = QtWidgets.QWidget()
+        input_group = QtWidgets.QButtonGroup(sg_input_widget)
+        input_group.setExclusive(True)
+
+        sg_playlist_id_input = QtWidgets.QComboBox()
+        sg_playlist_id_input.setToolTip("Integer id of the SG Playlist (i.e., '3909')")
+        playlist_radio_btn = QtWidgets.QRadioButton("SG Playlist Id")
+        playlist_radio_btn.setChecked(True)
+        input_group.addButton(playlist_radio_btn)
+        input_layout.addRow(playlist_radio_btn, sg_playlist_id_input)
+
+        sg_version_id_input = QtWidgets.QLineEdit()
+        sg_version_id_input.setToolTip("Integer id of the SG Version (i.e., '314726')")
+        sg_version_id_input.textEdited.connect(self._version_id_edited)
+        version_radio_btn = QtWidgets.QRadioButton("SG Version Id")
+        input_group.addButton(version_radio_btn)
+        input_layout.addRow(version_radio_btn, sg_version_id_input)
+
+        main_layout.addWidget(sg_input_widget)
+
+        main_layout.addWidget(input_widget)
+
+        # Add button to generate delivery media
+        outsource_delivery_btn = QtWidgets.QPushButton(
+            "Deliver for outsource"
+        )
+        outsource_delivery_btn.setDefault(True)
+        outsource_delivery_btn.setToolTip(
+            "Run the outsource delivery pipeline so it copies the SG entities into"
+            " a package in the '/proj/<proj_code>/io/outsource/ready_to_deliver' folder."
+        )
+        outsource_delivery_btn.clicked.connect(
+            self._on_outsource_delivery_clicked
+        )
+
+        main_layout.addWidget(outsource_delivery_btn)
+
+        #### REPORT ####
+        text_area = QtWidgets.QTextEdit()
+        text_area.setReadOnly(True)
+        text_area.setVisible(False)
+
+        main_layout.addWidget(text_area)
+
+        # Assign widgets we want to reuse to class instance
+
+        self._projects_combobox = projects_combobox
+        self._sg_playlist_id_input = sg_playlist_id_input
+        self._sg_playlist_btn = playlist_radio_btn
+        self._sg_version_id_input = sg_version_id_input
+        self._sg_version_btn = version_radio_btn
+        self._text_area = text_area
+
+    def showEvent(self, event):
+        super(OutsourceDialog, self).showEvent(event)
+        if self._first_show:
+            self._first_show = False
+            self.setStyleSheet(style.load_stylesheet())
+            tools_lib.center_window(self)
+
+        if not self._initial_refresh:
+            self._initial_refresh = True
+            self.refresh()
+
+    def _version_id_edited(self, text):
+        # If there's a comma in the text, remove it and set the modified text
+        text = text.replace("\t", "")
+        text = text.replace(" ", "")
+        text = text.replace(",", "")
+        self._sg_version_id_input.setText(text)
+        self._sg_version_btn.setChecked(True)
+
+    def _refresh(self):
+        if not self._initial_refresh:
+            self._initial_refresh = True
+        self._set_projects()
+
+    def _set_projects(self):
+        # Store current project
+        old_project_name = self.current_project
+
+        self._ignore_project_change = True
+
+        # Cleanup
+        self._projects_combobox.clear()
+
+        # Fill combobox with projects
+        select_project_item = QtGui.QStandardItem("< Select project >")
+        select_project_item.setData(None, QtCore.Qt.UserRole + 1)
+
+        combobox_items = [select_project_item]
+
+        project_names = self.get_filtered_projects()
+
+        for project_name in sorted(project_names):
+            item = QtGui.QStandardItem(project_name)
+            item.setData(project_name, QtCore.Qt.UserRole + 1)
+            combobox_items.append(item)
+
+        root_item = self._projects_combobox.model().invisibleRootItem()
+        root_item.appendRows(combobox_items)
+
+        index = 0
+        self._ignore_project_change = False
+
+        if old_project_name:
+            index = self._projects_combobox.findText(
+                old_project_name, QtCore.Qt.MatchFixedString
+            )
+
+        self._projects_combobox.setCurrentIndex(index)
+
+    @property
+    def current_project(self):
+        return self.dbcon.active_project() or None
+
+    def get_filtered_projects(self):
+        projects = list()
+        for project in get_projects(fields=["name"]):
+            projects.append(project["name"])
+
+        return projects
+
+    def on_project_change(self):
+        if self._ignore_project_change:
+            return
+
+        row = self._projects_combobox.currentIndex()
+        index = self._projects_combobox.model().index(row, 0)
+        project_name = index.data(QtCore.Qt.UserRole + 1)
+
+        self.dbcon.Session["AVALON_PROJECT"] = project_name
+
+        sg_project = self.sg.find_one(
+            "Project",
+            [["name", "is", project_name]],
+            ["sg_code"]
+        )
+
+        project_name = self.dbcon.active_project() or "No project selected"
+        title = "{} - {}".format(self.tool_title, project_name)
+        self.setWindowTitle(title)
+
+        # Find project code from SG project and load config file if it exists
+        proj_code = sg_project.get("sg_code")
+
+        # Store project code as class variable so we can reuse it throughout
+        self._current_proj_code = proj_code
+
+        # Add existing playlists from project
+        sg_playlists = self.sg.find(
+            "Playlist",
+            [["project", "is", sg_project]],
+            ["id", "code"]
+        )
+        self._sg_playlist_id_input.clear()
+        if sg_playlists:
+            playlist_items = [
+                "{} ({})".format(p["code"], p["id"])
+                for p in sg_playlists
+            ]
+            # Using reversed so they are ordered from newer to older
+            self._sg_playlist_id_input.addItems(reversed(playlist_items))
+
+    def _format_report(self, report_items, success):
+        """Format final result and error details as html."""
+        msg = "Delivery finished"
+        if success:
+            msg += " successfully"
+        else:
+            msg += " with errors"
+        txt = "<h2>{}</h2>".format(msg)
+        for header, data in report_items.items():
+            txt += "<h3>{}</h3>".format(header)
+            for item in data:
+                txt += "{}<br>".format(item)
+
+        return txt
+
+    def _on_outsource_delivery_clicked(self):
+
+        try:
+            if self._sg_playlist_btn.isChecked():
+                playlist_id_str = self._sg_playlist_id_input.currentText()
+                playlist_id = re.search(r"\((\d+)\)$", playlist_id_str).group(1)
+                report_items, success = sg_delivery.deliver_playlist_id(
+                    playlist_id
+                )
+            else:
+                report_items, success = sg_delivery.deliver_version_id(
+                    self._sg_version_id_input.text()
+                )
+        except Exception:
+            logger.error(traceback.format_exc())
+            report_items = {
+                "Error": [traceback.format_exc()]
+            }
+            success = False
+
+        self._text_area.setText(self._format_report(report_items, success))
+        self._text_area.setVisible(True)
+
+
+    # -------------------------------
+    # Delay calling blocking methods
+    # -------------------------------
+
+    def refresh(self):
+        tools_lib.schedule(self._refresh, 50, channel="mongo")
+
+
+def main():
+    app_instance = QtWidgets.QApplication.instance()
+    if app_instance is None:
+        app_instance = QtWidgets.QApplication([])
+
+    if platform.system().lower() == "windows":
+        import ctypes
+
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("sg_outsource_delivery")
+
+    window = OutsourceDialog()
+    window.show()
+
+    # Trigger on project change every time the tool loads
+    window.on_project_change()
+
+    app_instance.exec_()
