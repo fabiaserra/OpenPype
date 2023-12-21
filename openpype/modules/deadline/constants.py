f31d3bc447ed5e34db975e4d8b5704b97e27be8b
farrizabalaga <farrizabalaga@alkemy-x.com>
2023-12-21 17:17:36 -0500

    Update to new deadline webserver

--- openpype/modules/deadline/constants.py
+++ openpype/modules/deadline/constants.py
@@ -1,12 +1,12 @@
 
 # URL for REST API of Deadline
-DEADLINE_URL = "http://webserver-deadline-10-1-23-6:8082"
+DEADLINE_URL = "http://webserver-deadline-10-2-1-1:8082"
 
 # Name of AlkemyX Deadline group used for Nuke CPU tasks
-NUKE_CPU_GROUP = "nuke-cpu-epyc"
+NUKE_CPU_GROUP = "nuke-cpu"
 
 # Name of AlkemyX Deadline group used for Houdini CPU tasks
-HOUDINI_CPU_GROUP = "houdini-cpu-epyc"
+HOUDINI_CPU_GROUP = "houdini-cpu"
 
 # Name of AlkemyX Deadline group for OpenPype tasks
 OP_GROUP = "openpype"
