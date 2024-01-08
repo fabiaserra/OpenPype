3aed81648ad6b5502e089df3f25f8240eb0ce8bb
farrizabalaga <farrizabalaga@alkemy-x.com>
2024-01-08 09:52:00 -0700

    Revert changes to groups

--- openpype/modules/deadline/constants.py
+++ openpype/modules/deadline/constants.py
@@ -3,19 +3,19 @@
 DEADLINE_URL = "http://webserver-deadline-10-2-1-1:8082"
 
 # Name of AlkemyX Deadline group used for Nuke CPU tasks
-NUKE_CPU_GROUP = "nuke-cpu-{}-{}--12cpu-64gb"
+NUKE_CPU_GROUP = "nuke-cpu-{}-{}"
 
 # Name of AlkemyX Deadline group used for Nuke GPU tasks
-NUKE_GPU_GROUP = "nuke-gpu-{}-{}--24cpu-64gb-a4000"
+NUKE_GPU_GROUP = "nuke-gpu-{}-{}"
 
 # Name of AlkemyX Deadline group used for Houdini CPU tasks
-HOUDINI_CPU_GROUP = "houdini-cpu-{}-{}--70cpu-180gb"
+HOUDINI_CPU_GROUP = "houdini-cpu-{}-{}"
 
 # Name of AlkemyX Deadline group used for Houdini GPU tasks
-HOUDINI_GPU_GROUP = "houdini-gpu-{}-{}--24cpu-64gb-a4000"
+HOUDINI_GPU_GROUP = "houdini-gpu-{}-{}"
 
 # Name of AlkemyX Deadline group for OpenPype tasks
-OP_GROUP = "openpype-3-16--12cpu-64gb"
+OP_GROUP = "openpype-3-16"
 
 # Name of AlkemyX Deadline group for Ayon tasks
-AYON_GROUP = "ayon-1-0--12cpu-64gb"
+AYON_GROUP = "ayon-1-0"
