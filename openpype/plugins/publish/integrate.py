48cfa471ee5ae1253396346a01c24c8e57ed82f1
Fabia Serra Arrizabalaga <farrizabalaga@alkemy-x.com>
2024-02-19 14:26:14 -0500

    Add stagingDir to representation data

--- openpype/plugins/publish/integrate.py
+++ openpype/plugins/publish/integrate.py
@@ -839,6 +839,9 @@ class IntegrateAsset(pyblish.api.InstancePlugin):
         data = repre.get("data", {})
         data.update({"path": published_path, "template": template})
 
+        # Add stagingDir to representation data
+        data["stagingDir"] = stagingdir
+
         # add colorspace data if any exists on representation
         if repre.get("colorspaceData"):
             data["colorspaceData"] = repre["colorspaceData"]
