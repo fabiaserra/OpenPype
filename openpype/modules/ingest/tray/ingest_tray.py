f4128738dc490e62ba9d261882c55effb76392e5
farrizabalaga <farrizabalaga@alkemy-x.com>
2024-03-22 23:37:18 +0100

    Add support to launch texture publisher from tray

--- openpype/modules/ingest/tray/ingest_tray.py
+++ openpype/modules/ingest/tray/ingest_tray.py
@@ -3,7 +3,8 @@
 from qtpy import QtWidgets
 
 from openpype.lib import Logger
-from openpype.modules.ingest.tray.ingest_dialog import IngestDialog
+from openpype.modules.ingest.tray.batch_ingester import BatchIngester
+from openpype.modules.ingest.tray.texture_publisher import TexturePublisher
 
 
 class IngestTrayWrapper:
@@ -11,18 +12,28 @@ class IngestTrayWrapper:
         self.module = module
         self.log = Logger.get_logger(self.__class__.__name__)
 
-        self.delivery_dialog = IngestDialog(module)
+        self.ingest_dialog = BatchIngester(module)
+        self.texture_publisher = TexturePublisher(module)
 
     def show_ingest_dialog(self):
-        self.delivery_dialog.show()
-        self.delivery_dialog.activateWindow()
-        self.delivery_dialog.raise_()
+        self.ingest_dialog.show()
+        self.ingest_dialog.activateWindow()
+        self.ingest_dialog.raise_()
+
+    def show_texture_dialog(self):
+        self.texture_publisher.show()
+        self.texture_publisher.activateWindow()
+        self.texture_publisher.raise_()
 
     def tray_menu(self, parent_menu):
         tray_menu = QtWidgets.QMenu("Ingest", parent_menu)
 
-        show_delivery_action = QtWidgets.QAction("Ingest products", tray_menu)
-        show_delivery_action.triggered.connect(self.show_ingest_dialog)
-        tray_menu.addAction(show_delivery_action)
+        batch_ingest_action = QtWidgets.QAction("Batch Ingester", tray_menu)
+        batch_ingest_action.triggered.connect(self.show_ingest_dialog)
+        tray_menu.addAction(batch_ingest_action)
+
+        texture_publish_action = QtWidgets.QAction("Texture Publisher", tray_menu)
+        texture_publish_action.triggered.connect(self.show_texture_dialog)
+        tray_menu.addAction(texture_publish_action)
 
         parent_menu.addMenu(tray_menu)
