19fe567609be5635e3ac68e728c80acc3ad9d20d
farrizabalaga <farrizabalaga@alkemy-x.com>
2024-05-24 00:19:05 +0200

    Add subset name to review generation to avoid collision on multi instance publish

--- openpype/modules/deadline/lib/publish.py
+++ openpype/modules/deadline/lib/publish.py
@@ -349,7 +349,7 @@ def publish_version(
             logger.debug("Review read path: %s", read_path)
 
             # Create review output path
-            file_name = f"{review_repre['name']}_h264.mov"
+            file_name = f"{subset_name}_{review_repre['name']}_h264.mov"
             output_path = os.path.join(
                 staging_dir,
                 file_name
